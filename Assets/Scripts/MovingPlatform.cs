24280ba11c3d221a1384272ab13529e54d0621fc
mparrauoc <marcparra@uoc.edu>
2021-05-05 00:35:10 +0200

    Moving Platform + Falling Platform

---
+++ Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    public Transform pointA;
+    public Transform pointB;
+    public float speed;
+    public Transform startPoint;
+    Vector3 nextPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nextPoint = startPoint.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position == pointA.position)
+        {
+            nextPoint = pointB.position;
+        }
+        if (transform.position == pointB.position)
+        {
+            nextPoint = pointA.position;
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, nextPoint, speed * Time.deltaTime);
+    }
+
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(pointA.position, pointB.position);
+    }
+}
